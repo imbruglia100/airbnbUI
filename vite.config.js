f95bdf0942e8eef273d3ead7ede47c894bce41a5
frank <imbruglia100@gmail.com>
2024-06-11 19:21:29 -0400

    Render push

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
-export default defineConfig(() => ({
+export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     eslint({
       lintOnStart: true,
-      failOnError: "production"
+      failOnError: mode === "production"
     })
   ],
 
